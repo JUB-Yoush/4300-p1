85fd9a2dbeed640f89cc4d1a6d23ceaf40912166
Lusa <jasminely428@gmail.com>
2025-09-15 13:20:49 -0400

    ExitScreen implemented

---
+++ src/scenes/scripts/EndScreenManager.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class EndScreenManager : Node
+{
+    TextureButton mainBtn;
+    TextureButton exitBtn;
+    AnimationPlayer animPlayer;
+ 
+
+    public override void _Ready()
+    {
+      
+        mainBtn = GetNode<TextureButton>("HBoxContainer/Main");
+        exitBtn = GetNode<TextureButton>("HBoxContainer/Exit");
+      
+
+       
+        mainBtn.Disabled = true;
+        exitBtn.Disabled = true;
+
+    
+
+        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        animPlayer.Play("end_screen");
+        animPlayer.AnimationFinished += AnimPlayer_AnimationFinished;
+
+        exitBtn.Pressed += OnExitPressed;
+        mainBtn.Pressed += OnMainPressed;
+    }
+
+    private void AnimPlayer_AnimationFinished(StringName animName)
+    {
+        GD.Print("Animation done");
+       if (animName == "end_screen")
+        {
+            mainBtn.Disabled = false;
+            exitBtn.Disabled = false;
+        }
+    }
+
+    private void OnExitPressed()
+    {
+        GetTree().Quit();
+    }
+
+    private void OnMainPressed()
+    {
+        GetTree().ChangeSceneToFile("res://src/scenes/title.tscn");
+    }
+}
